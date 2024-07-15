92695fbd9fe51d4a0e73b7486a38ed2d981e8be2
hsyntes <se.hsyntes@gmail.com>
2024-07-15 10:39:15 +0300

    improved fething stories logic

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const storage = multer({ storage: multer.memoryStorage() });
-
 const { verifyToken } = require("../middlewares/auth.middleware");
 
 const {
